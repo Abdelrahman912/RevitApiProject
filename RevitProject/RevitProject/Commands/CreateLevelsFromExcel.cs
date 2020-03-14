1f55a0a95d1892e1b013b899ffac5592817df823
abdelrahman fathy <abdelrahmanfathe95@gmail.com>
2020-03-14 11:13:07 +0200

    organizing project into three layers (command , Logic , revit)

---
+++ RevitProject/RevitProject/Commands/CreateLevelsFromExcel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+using System.Linq;
+using System.IO;
+using OfficeOpenXml;
+using RevitLogic;
+
+namespace RevitCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    class CreateLevelsFromExcel : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                FromExcelToRevit e2r = new FromExcelToRevit(commandData, "revitSheet", "revitData");
+                using (Transaction trans = new Transaction(e2r.Revit.Doc, "Create Levels and views"))
+                {
+                    bool flag = true;
+                    trans.Start();
+                    flag = e2r.CreateLevelsAndViewPlans();
+                    trans.Commit();
+                    if (!flag)
+                    {
+                        return Result.Failed;
+                    }
+
+                    return Result.Succeeded;
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                message = e.Message;
+                return Result.Failed;
+            }
+          
+        }
+    }
+}
---
+++ RevitProject/RevitProject/Logic/FromExcelToRevit.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using ExcelIO;
+using Revit;
+
+namespace RevitLogic
+{
+  public  class FromExcelToRevit
+    {
+        #region Member Variables
+        private Excel excel;
+        private  List<ExcelData> data;
+        RevitApp revit;
+        #endregion
+
+        #region Constructors
+        public FromExcelToRevit(ExternalCommandData commandData,string workSheetName,string tableName)
+        {
+            Revit = new RevitApp(commandData);
+            FileOpenDialog dialog = new FileOpenDialog("Excel Files(*.xlsx) | *.xlsx");
+            if (dialog.Show() == ItemSelectionDialogResult.Confirmed)
+            {
+                string path = ModelPathUtils.ConvertModelPathToUserVisiblePath(dialog.GetSelectedModelPath());
+                 excel = new Excel(path);
+                data = new List<ExcelData>();
+                data = excel.ReadFromTable<ExcelData>(workSheetName, tableName, (v, i) =>
+                {
+                    return new ExcelData(v[i, 0].ToString(), v[i, 1].ToString(), v[i, 2].ToString());
+                });
+            }
+            else
+            {
+                data = null;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public RevitApp Revit { get => revit; set => revit = value; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Create Levels and Floor Plans from excel sheet
+        /// </summary>
+        /// <returns></returns>
+        public bool CreateLevelsAndViewPlans()
+        {
+            bool flag = true;
+
+            if (data != null)
+            {
+                Level level;
+                ViewFamilyType viewType = Revit.SelectClassBased<ViewFamilyType>(true, x => x.ViewFamily == ViewFamily.FloorPlan); //select floor plan type only
+                foreach (var d in data)
+                {
+                    level = Level.Create(Revit.Doc, UnitUtils.ConvertToInternalUnits(Convert.ToDouble(d.Level), DisplayUnitType.DUT_METERS));
+
+                    Revit.CreatePlanView(level.Name, d.ViewPlan);
+                }
+
+            }
+            else
+            {
+                flag = false;
+            }
+
+
+
+            return flag;
+        }
+        #endregion
+
+    }
+}
